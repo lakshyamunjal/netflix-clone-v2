9d9e1e9fbb11430b209570d29bbb15b56853a90c
Lakshya Munjal <lakshyamunjal27@gmail.com>
2023-03-24 14:31:22 +0530

    initialize firebase

---
+++ src/firebase.ts
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCthAtDX6SgH5ytj8jQfMcaarcBC3KkNQo",
+  authDomain: "netflix-clone-v2-3aa74.firebaseapp.com",
+  projectId: "netflix-clone-v2-3aa74",
+  storageBucket: "netflix-clone-v2-3aa74.appspot.com",
+  messagingSenderId: "712454777201",
+  appId: "1:712454777201:web:e14356d04472c3ba5b6ed0",
+  measurementId: "G-JYKJW9DSZS"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
