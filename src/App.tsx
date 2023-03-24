66c111e7aaec9cd7925602dc37ca0e94aabc749d
Lakshya Munjal <lakshyamunjal27@gmail.com>
2023-03-24 18:11:55 +0530

    applied custom font throughtout the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from "react";
 
-import { StoreProvider } from "./stores";
-import { rootStore } from './stores/root-store';
+import HomeScreen from "./screens/home/HomeScreen";
 
-import "./App.css";
+import { StoreProvider } from "./stores";
+import { rootStore } from "./stores/root-store";
+
+import "./App.scss";
 
 function App() {
   return (
     <StoreProvider value={rootStore}>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app">
+        <HomeScreen />
       </div>
     </StoreProvider>
   );
