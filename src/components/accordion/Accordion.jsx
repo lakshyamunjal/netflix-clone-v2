8623ef447078aac526bd5ef5eaffc98403be7b09
Lakshya Munjal <lakshyamunjal27@gmail.com>
2023-09-02 13:12:37 +0530

    [023] Used Accordion from '@mui'

--- src/components/accordion/Accordion.jsx
+++ src/components/accordion/Accordion.jsx
@@ -1,48 +1,62 @@
 import React from "react";
+import {
+  Accordion as MuiAccordion,
+  AccordionSummary,
+  AccordionDetails,
+  styled,
+} from "@mui/material";
 
 import "./accordion-styles.scss";
+
+const StyledAccordion = styled((props) => (
+  <MuiAccordion disableGutters elevation={0} square {...props} />
+))(({ theme }) => ({
+  backgroundColor: "#2d2d2d",
+  border: `1px solid ${theme.palette.divider}`,
+  fontSize: '22px',
+  paddingTop: '10px',
+  paddingBottom: '10px',
+  marginBottom: '10px',
+  "&:not(:last-child)": {
+    borderBottom: 0,
+  },
+  "&:before": {
+    display: "none",
+  },
+}));
+
+const StyledAccordionSummary = styled((props) => (
+  <AccordionSummary
+    expandIcon={props.isExpanded ? <span>-</span> : <span>+</span>}
+    {...props}
+  />
+))(({ theme }) => ({
+  "& .MuiAccordionSummary-content": {
+    marginLeft: theme.spacing(1), 
+  },
+}));
+
+const StyledAccordionDetails = styled(AccordionDetails)(({ theme }) => ({
+  padding: theme.spacing(3),
+  borderTop: "1px solid rgba(0, 0, 0, .125)",
+}));
 
 const Accordion = (props) => {
   const {
     answer,
-    answerStyle,
-    CloseIcon,
-    containerStyle,
     index,
     isExpanded,
     onClick,
-    OpenIcon,
     question,
-    questionStyle,
   } = props;
 
-  const AccordionOpenIcon = OpenIcon ? (
-    <OpenIcon />
-  ) : (
-    <span className="icon">+</span>
-  );
-  const AccordionCloseIcon = CloseIcon ? (
-    <CloseIcon />
-  ) : (
-    <span className="icon">-</span>
-  );
-
   return (
-    <div style={containerStyle} className="accordion">
-      <div onClick={onClick(index)} className="question-container">
-        <span style={questionStyle} className="question">
-          {question}
-        </span>
-        {isExpanded ? AccordionCloseIcon : AccordionOpenIcon}
-      </div>
-      {isExpanded && (
-        <div style={answerStyle} className="answer-container">
-          <div className="answer" style={answerStyle}>
-            {answer}
-          </div>
-        </div>
-      )}
-    </div>
+    <StyledAccordion expanded={isExpanded} onChange={onClick(index)}>
+      <StyledAccordionSummary isExpanded={isExpanded}>
+        <span>{question}</span>
+      </StyledAccordionSummary>
+      <StyledAccordionDetails>{answer}</StyledAccordionDetails>
+    </StyledAccordion>
   );
 };
 
--- src/components/faqs/Faqs.jsx
+++ src/components/faqs/Faqs.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
+
 import Accordion from "../accordion/Accordion";
 
 import FAQS, { TRANSLATION_PREFIX } from "./constants";
